5bbc742323146fca109963c68f57549eea4769d9
MS8oo8 <martasieradzka68@gmail.com>
2025-11-16 20:33:39 +0100

    Zmiana w parameters.py
    Na ta chwile nie usuwuam tutaj zakomentownaych funkcji.
    W teorii ris_ports.json nie jest potrzebny on powienien skanowac porty i przypisywac cos jak w RX jest zriobione dodatkowo uporzadkowany generator

--- helpers/parameters.py
+++ helpers/parameters.py
@@ -9,6 +9,8 @@ import pandas as pd
 from datetime import datetime
 from loguru import logger as log
 import zipfile
+from serial.tools import list_ports
+
 
 
 
@@ -16,44 +18,70 @@ class GeneratorModel(str, Enum):
     #SMM100A = 'SMM100A' 
     SMBV100A = 'SMBV100A' 
 
-
+DEFAULT_GENERATOR_IP =  "192.168.8.30"
+DEFAULT_GENERATOR_PORT = 5025
 DEFAULT_GENERATOR_MODEL = GeneratorModel.SMBV100A
 
-class GeneratorConnection(BaseModel):
-    mode: str = "wlan"  # "dvbt"
-    generator_model: GeneratorModel = DEFAULT_GENERATOR_MODEL
-    ip_address: str = ""
-    transmit_power: float = -20.0
-    transmission_enabled: bool =  True
+
+class GeneratorSettings(BaseModel):
     frequency: float = 5e9
-    port: int = 5025
+    transmit_power: float = -20.0
+    enabled: bool = True
+    
+class GeneratorConnection(BaseModel):
+    model: GeneratorModel = DEFAULT_GENERATOR_MODEL
+    ip: str = DEFAULT_GENERATOR_IP
+    port: int = DEFAULT_GENERATOR_PORT
+    mode: str = "wlan"
     connection_type: str = "SOCKET"
 
-    model_config = {
-        "arbitrary_types_allowed" : True
-    }
+class GeneratorConfig(BaseModel):
+    settings: GeneratorSettings = GeneratorSettings()
+    connection: GeneratorConnection = GeneratorConnection()
+    
+    @property
+    def address(self) -> str:
+        return f"{self.connection.ip}:{self.connection.port}"
+    
+    @property
+    def model(self) -> GeneratorModel:
+        return self.connection.model
+
+# class GeneratorConnection(BaseModel):
+#     mode: str = "wlan"  # "dvbt"
+#     generator_model: GeneratorModel = DEFAULT_GENERATOR_MODEL
+#     ip_address: str = ""
+#     transmit_power: float = -20.0
+#     transmission_enabled: bool =  True
+#     frequency: float = 5e9
+#     port: int = 5025
+#     connection_type: str = "SOCKET"
+
+#     model_config = {
+#         "arbitrary_types_allowed" : True
+#     }
 
 
-class GeneratorParams(BaseModel):
-    model: GeneratorModel
-    connection: Optional[GeneratorConnection] = None
+# class GeneratorParams(BaseModel):
+#     model: GeneratorModel
+#     connection: Optional[GeneratorConnection] = None
 
-    def __init__(self, **data):
-        super().__init__(**data)
-        if not self.connection:
-            ip_map = {
-                DEFAULT_GENERATOR_MODEL: "192.168.8.30",
-                #GeneratorModel.SMBV100A: "192.168.8.30"
-                #GeneratorModel.SMM100A: "192.168.8.160"
+#     def __init__(self, **data):
+#         super().__init__(**data)
+#         if not self.connection:
+#             ip_map = {
+#                 DEFAULT_GENERATOR_MODEL: "192.168.8.30",
+#                 #GeneratorModel.SMBV100A: "192.168.8.30"
+#                 #GeneratorModel.SMM100A: "192.168.8.160"
 
-            }
-            self.connection = GeneratorConnection(
-                generator_model=self.model,
-                ip_address=ip_map.get(self.model, "192.168.8.30")
-            )
-    model_config = {
-        "arbitrary_types_allowed" : True
-    }
+#             }
+#             self.connection = GeneratorConnection(
+#                 generator_model=self.model,
+#                 ip_address=ip_map.get(self.model, "192.168.8.30")
+#             )
+#     model_config = {
+#         "arbitrary_types_allowed" : True
+#     }
 
 
 
@@ -61,7 +89,6 @@ class GeneratorParams(BaseModel):
 class RxParams(BaseModel):
     samp_rate: float = 500e3 # 8e6
     rx_gain: float = 40.0
-    #tutaj ustawiona ilość RXow
     count: int = 1
     buffer_size: int = int(40e3) #32768 #int(1e6) #1024
     N: int = 1
@@ -82,6 +109,10 @@ class RisParams(BaseModel):
     pattern: str = None
     index: int = None
 
+RIS_SERIAL_MAP = {
+    "0":"/dev/ttyUSB0",
+    "1":"/dev/ttyUSB1"
+}
 
 
 class Params(BaseModel):
@@ -96,7 +127,8 @@ class Params(BaseModel):
         return args
     
     frequency: float = 5e9
-    generator: GeneratorParams = GeneratorParams(model = DEFAULT_GENERATOR_MODEL)
+    #generator: GeneratorParams = GeneratorParams(model = DEFAULT_GENERATOR_MODEL)
+    generator: GeneratorConfig = GeneratorConfig()
     rxes: RxParams = RxParams()
     usrp: UsrpParams = UsrpParams()
     rises: Dict[str, RisParams] = Field(default={
@@ -115,20 +147,40 @@ class Parameters(metaclass=SingletonMeta):
 
     def __init__(self):
         self.data = Params()
-        self._ris_port_map = {}
-        # self._ris_available_ports =  self._scan_usb_ports()
+        self._ris_port_map = self._scan_usb_ports()
+        #self._ris_available_ports =  self._scan_usb_ports()
 
-                # #log przypisanych portów
-                        # for ris_id in self.data.rises:
-                                #     try:
-                                        #         port = self.get_ris_port(ris_id)
-                                                #         log.info("RIS {} przypisany do portu: {}", ris_id, port)
-                                                        #     except RuntimeError as e:
-                                                                #         log.error("Błąd przypisania portu do RIS {}: {}", ris_id, e)
+        for ris_id in self.data.rises:
+            try:
+                port = self.get_ris_port(ris_id)
+                log.info("RIS {} przypisany do portu: {}", ris_id, port)
+            except RuntimeError as e:
+                log.error("Błąd przypisania portu do RIS {}: {}", ris_id, e)
 
     def get(self):
         return self.data
-
+    
+    def get_ris_port(self, component_id: str) -> str:
+        if component_id not in self._ris_port_map:
+            raise RuntimeError(
+                f'Bral portu USB dla RIS {component_id}.'
+                f"Sprawdz numer seryjny w RIS_SERIAL_MAP."
+            )
+        return self._ris_port_map[component_id]
+    
+    def _scan_usb_ports(self):
+        ports = list_ports.comports()
+        result = {}
+        for p in ports:
+            if p.serial_number is None:
+                continue
+            
+            for ris_id, excepted_serial in RIS_SERIAL_MAP.items():
+                if p.serial_number == excepted_serial:
+                    result[ris_id] = p.device
+                    log.info(f"RIS {ris_id} wykryty na porcie {p.device} (serial = {p.serial_number})")
+                    
+        return result
     # def get_ris_port(self, component_id: str) -> str:
     #     if component_id in self._ris_port_map:
     #         return self._ris_port_map[component_id]
